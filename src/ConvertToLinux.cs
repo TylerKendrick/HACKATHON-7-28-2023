f076de131523481d1c77d353baf12765f4fc9bd1
Tyler Kendrick <244215+TylerKendrick@users.noreply.github.com>
2023-07-28 19:28:38 +0000

    added converter

--- src/ConvertToLinux.cs
+++ src/ConvertToLinux.cs
@@ -7,29 +7,53 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.AI.OpenAI;
+using Azure;
 
-namespace src
+namespace src;
+public static class ConvertToLinux
 {
-    public static class ConvertToLinux
+    private const string
+        openaiUri = "https://your-azure-openai-resource.com",
+        azureKeyCredential = "your-azure-openai-resource-api-key";
+
+
+    [FunctionName("ConvertToLinux")]
+    public static async Task<IActionResult> RunLinux(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        [FunctionName("ConvertToLinux")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+        string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+        string responseMessage = string.IsNullOrEmpty(name)
+            ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
-        }
+        return new OkObjectResult(responseMessage);
+    }
+
+    [FunctionName("ConvertToWindows")]
+    public static async Task<IActionResult> RunWindows(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+        OpenAIClient client = new(
+                new Uri(openaiUri),
+                new AzureKeyCredential(azureKeyCredential));
+
+        string prompt = $"Convert to windows shell commands: {req.Body}";
+        Response<Completions> response = await client.GetCompletionsAsync(
+            "gpt-35-turbo", // assumes a matching model deployment or model name
+            prompt);
+
+        var responseBody = string.Join(Environment.NewLine, response.Value.Choices);
+        return new OkObjectResult(responseBody);
     }
 }
